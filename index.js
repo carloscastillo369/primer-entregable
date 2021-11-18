5a70e7050160b690182b5aabbb1c7dae6a32edeb
Carlos Castillo Cueva <ing.elec.castillo@gmail.com>
2021-11-17 22:04:19 -0500

    creación logaritmo de un cajero automático

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-//Primer entregable
+//Primer entregable - Cajero Automático
+var Client = /** @class */ (function () {
+    function Client(nombre, apellido, password, saldo) {
+        var _this = this;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.password = password;
+        this.saldo = saldo;
+        this.verificarContraseña = function (contraseña) {
+            if (_this.password === contraseña) {
+                console.log("Bienvenid@ " + _this.nombre + " " + _this.apellido);
+            }
+            else {
+                console.log('Contraseña inválida');
+            }
+        };
+        this.obtenerSaldo = function () {
+            console.log("Tu saldo es: " + _this.saldo + " soles");
+        };
+        this.retirarDinero = function (retiro) {
+            console.log("Has retirado: " + retiro + " soles. Tu saldo es: " + (_this.saldo - retiro) + " soles");
+        };
+    }
+    return Client;
+}());
+var carlos = new Client('Carlos', 'Castillo', 1111, 1500);
+var jesus = new Client('Jesus', 'Ramos', 1234, 3000);
+var diego = new Client('Diego', 'Perez', 5555, 1000);
+var andrea = new Client('Andrea', 'Silva', 2233, 600);
+andrea.verificarContraseña(2233);
+andrea.obtenerSaldo();
+andrea.retirarDinero(125);
--- index.ts
+++ index.ts
@@ -1,1 +1,40 @@
-//Primer entregable
+//Primer entregable - Cajero Automático
+
+class Client {
+
+    constructor(
+        public nombre:string,
+        public apellido:string,
+        public password:number,
+        public saldo:number
+    ){ 
+
+    }
+
+    public verificarContraseña =(contraseña:number)=>{
+        if(this.password === contraseña){
+            console.log(`Bienvenid@ ${this.nombre} ${this.apellido}`);
+        } 
+        else {
+            console.log('Contraseña inválida');
+        }
+    }
+
+    public obtenerSaldo = ()=>{ 
+        console.log(`Tu saldo es: ${this.saldo} soles`)
+    }
+
+    public retirarDinero = (retiro:number)=>{
+        console.log(`Has retirado: ${retiro} soles. Tu saldo es: ${this.saldo - retiro} soles`)
+    }
+}
+
+let carlos = new Client('Carlos', 'Castillo', 1111, 1500);
+let jesus = new Client('Jesus', 'Ramos', 1234, 3000);
+let diego = new Client('Diego', 'Perez', 5555, 1000);
+let andrea = new Client('Andrea', 'Silva', 2233, 600);
+
+
+andrea.verificarContraseña(2233);
+andrea.obtenerSaldo();
+andrea.retirarDinero(125);
